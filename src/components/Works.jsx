d07054a0518c5c25514ccbba14bce7a3c70d613c
Nikola-Obradovic <113454455+Nikola-Obradovic@users.noreply.github.com>
2024-02-28 02:32:38 +0100

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -29,7 +29,6 @@ const ProjectCard = ({
         <div className="relative w-full h-[230px]">
           <img
             src={image}
-            alt="project_image"
             className="w-full h-full object-cover rounded-2xl"
           />
 
